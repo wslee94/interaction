0b75f07027bb886bdfbc7424a3b5aff552d0e507
wslee94 <leemuo123@gmail.com>
2023-03-19 21:39:00 +0900

    Slider class화

--- slider/main.js
+++ slider/main.js
@@ -1,140 +1,34 @@
-const list = [
-  { image: "https://via.placeholder.com/640x480", text: 1 },
-  { image: "https://via.placeholder.com/640x480", text: 2 },
-  { image: "https://via.placeholder.com/640x480", text: 3 },
-  { image: "https://via.placeholder.com/640x480", text: 4 },
-  { image: "https://via.placeholder.com/640x480", text: 5 },
-  { image: "https://via.placeholder.com/640x480", text: 6 },
-  { image: "https://via.placeholder.com/640x480", text: 7 },
-  { image: "https://via.placeholder.com/640x480", text: 8 },
-  { image: "https://via.placeholder.com/640x480", text: 9 },
-  { image: "https://via.placeholder.com/640x480", text: 10 },
-  { image: "https://via.placeholder.com/640x480", text: 11 },
-  { image: "https://via.placeholder.com/640x480", text: 12 },
-  { image: "https://via.placeholder.com/640x480", text: 13 },
-  // { image: "https://via.placeholder.com/640x480", text: 14 },
-  // { image: "https://via.placeholder.com/640x480", text: 15 },
-  // { image: "https://via.placeholder.com/640x480", text: 16 },
-  // { image: "https://via.placeholder.com/640x480", text: 17 },
-  // { image: "https://via.placeholder.com/640x480", text: 18 },
-  // { image: "https://via.placeholder.com/640x480", text: 19 },
-  // { image: "https://via.placeholder.com/640x480", text: 20 },
-  // { image: "https://via.placeholder.com/640x480", text: 21 },
-  // { image: "https://via.placeholder.com/640x480", text: 22 },
-  // { image: "https://via.placeholder.com/640x480", text: 23 },
-  // { image: "https://via.placeholder.com/640x480", text: 24 },
-  // { image: "https://via.placeholder.com/640x480", text: 25 },
-  // { image: "https://via.placeholder.com/640x480", text: 26 },
-  // { image: "https://via.placeholder.com/640x480", text: 27 },
-];
+export default class Slider {
+  constructor(container, { data, speed, responsive }) {
+    this.container = container;
+    this.data = data;
+    this.speed = speed; // 슬라이드 동작 속도 (ms)
+    this.responsive = responsive; // 반응형 옵션
+    this.focusIndex = 0; // 현재 슬라이드 화면에서 맨 좌측 요소의 인덱스
+    this.lastIndex = data.length - 1; // 슬라이드 맨 우측 요소의 인덱스
+    this.bundleSize = 5; // 리스트에서 한번에 보여줄 카드 개수
+    this.cardWidth = 100 / this.bundleSize; // 카드 가로 길이 (%)
+    this.totalMoveDistance = 0; // 총 이동 거리 (%)
+    this.enableClick = true;
 
-const ul = document.querySelector("ul");
-const [prevBtn, nextBtn] = document.querySelectorAll(".btns button");
-
-const lastIndex = list.length - 1;
-
-let speed = 500;
-let enableClick = true;
-let focusIndex = 0; // 보고있는 리스트에서 맨 좌측 카드
-let bundleSize = 5; // 리스트에서 한번에 보여줄 카드 개수
-let cardWidth = 100 / bundleSize; // 카드 가로 길이 %
-let totalMoveDistance = 0; // 총 이동 거리 %
-
-prevBtn.addEventListener("click", prev);
-nextBtn.addEventListener("click", next);
-window.addEventListener("resize", resize);
-
-create();
-resize({ target: window });
-
-function resize(e) {
-  const { innerWidth } = e.target;
-  if (innerWidth > 1000) {
-    if (bundleSize === 5) return;
-    bundleSize = 5;
-  } else if (innerWidth > 800 && innerWidth <= 1000) {
-    if (bundleSize === 4) return;
-    bundleSize = 4;
-  } else if (innerWidth > 600 && innerWidth <= 800) {
-    if (bundleSize === 3) return;
-    bundleSize = 3;
-  } else if (innerWidth > 400 && innerWidth <= 600) {
-    if (bundleSize === 2) return;
-    bundleSize = 2;
-  } else {
-    if (bundleSize === 1) return;
-    bundleSize = 1;
-  }
-  cardWidth = 100 / bundleSize;
-  reset();
-}
-
-function reset() {
-  if (lastIndex - focusIndex + 1 >= bundleSize) {
-    totalMoveDistance = -(focusIndex * cardWidth);
-    ul.style.transform = `translateX(${totalMoveDistance}%)`;
-  } else {
-    focusIndex = lastIndex - bundleSize + 1;
-    totalMoveDistance = -(focusIndex * cardWidth);
-    ul.style.transform = `translateX(${totalMoveDistance}%)`;
-  }
-}
-
-function prev() {
-  if (!enableClick || focusIndex === 0) return;
-  enableClick = false;
-  setTimeout(() => {
-    enableClick = true;
-    ul.style.transitionDuration = "0s";
-  }, speed);
-
-  const nextFocusIndex = focusIndex - bundleSize;
-  const remainingCardCnt = focusIndex;
-
-  if (remainingCardCnt >= bundleSize) {
-    focusIndex = nextFocusIndex;
-    totalMoveDistance += 100;
-    ul.style.transitionDuration = `${speed}ms`;
-  } else {
-    focusIndex = 0;
-    totalMoveDistance += remainingCardCnt * cardWidth;
-    ul.style.transitionDuration = `${(remainingCardCnt / bundleSize) * speed}ms`;
+    this.init();
   }
 
-  ul.style.transform = `translateX(${totalMoveDistance}%)`;
-}
+  init() {
+    this.create();
 
-function next() {
-  if (!enableClick || focusIndex === lastIndex) return;
-  enableClick = false;
-
-  setTimeout(() => {
-    enableClick = true;
-    ul.style.transitionDuration = "0s";
-  }, speed);
-
-  const nextFocusIndex = focusIndex + bundleSize;
-  if (nextFocusIndex > lastIndex) return;
-
-  const remainingCardCnt = lastIndex - nextFocusIndex + 1;
-
-  if (remainingCardCnt >= bundleSize) {
-    focusIndex = nextFocusIndex;
-    totalMoveDistance -= 100;
-    ul.style.transitionDuration = `${speed}ms`;
-  } else {
-    focusIndex = lastIndex - bundleSize + 1;
-    totalMoveDistance -= remainingCardCnt * cardWidth;
-    ul.style.transitionDuration = `${(remainingCardCnt / bundleSize) * speed}ms`;
+    window.addEventListener("resize", () => {
+      const isChangedBundleSize = this.resize();
+      isChangedBundleSize && this.rearrange();
+    });
+    this.resize();
+    this.rearrange();
   }
 
-  ul.style.transform = `translateX(${totalMoveDistance}%)`;
-}
-
-function create() {
-  for (const item of list) {
-    const li = document.createElement("li");
-    li.innerHTML = `
+  create() {
+    for (const item of this.data) {
+      const li = document.createElement("li");
+      li.innerHTML = `
     <div class='wrapper'>
       <div class='img'>
         <img alt='' src=${item.image} />
@@ -144,6 +38,81 @@ function create() {
       </div>
     </div>
     `;
-    ul.appendChild(li);
+      this.container.appendChild(li);
+    }
+  }
+
+  prev() {
+    if (!this.enableClick || this.focusIndex === 0) return;
+    this.enableClick = false;
+    this.handleClickable();
+
+    const nextFocusIndex = this.focusIndex - this.bundleSize;
+    const remainingCardCnt = this.focusIndex;
+
+    if (remainingCardCnt >= this.bundleSize) {
+      this.focusIndex = nextFocusIndex;
+      this.totalMoveDistance += 100;
+      this.container.style.transitionDuration = `${this.speed}ms`;
+    } else {
+      this.focusIndex = 0;
+      this.totalMoveDistance += remainingCardCnt * this.cardWidth;
+      this.container.style.transitionDuration = `${(remainingCardCnt / this.bundleSize) * this.speed}ms`;
+    }
+
+    this.container.style.transform = `translateX(${this.totalMoveDistance}%)`;
+  }
+
+  next() {
+    if (!this.enableClick || this.focusIndex === this.lastIndex) return;
+    this.handleClickable();
+
+    const nextFocusIndex = this.focusIndex + this.bundleSize;
+    if (nextFocusIndex > this.lastIndex) return;
+
+    const remainingCardCnt = this.lastIndex - nextFocusIndex + 1;
+
+    if (remainingCardCnt >= this.bundleSize) {
+      this.focusIndex = nextFocusIndex;
+      this.totalMoveDistance -= 100;
+      this.container.style.transitionDuration = `${this.speed}ms`;
+    } else {
+      this.focusIndex = this.lastIndex - this.bundleSize + 1;
+      this.totalMoveDistance -= remainingCardCnt * this.cardWidth;
+      this.container.style.transitionDuration = `${(remainingCardCnt / this.bundleSize) * this.speed}ms`;
+    }
+
+    this.container.style.transform = `translateX(${this.totalMoveDistance}%)`;
+  }
+
+  resize() {
+    for (const option of this.responsive) {
+      if (option.condition(window.innerWidth) && this.bundleSize !== option.bundleSize) {
+        this.bundleSize = option.bundleSize;
+        this.cardWidth = 100 / option.bundleSize;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  rearrange() {
+    if (this.lastIndex - this.focusIndex + 1 >= this.bundleSize) {
+      this.totalMoveDistance = -(this.focusIndex * this.cardWidth);
+      this.container.style.transform = `translateX(${this.totalMoveDistance}%)`;
+    } else {
+      this.focusIndex = this.lastIndex - this.bundleSize + 1;
+      this.totalMoveDistance = -(this.focusIndex * this.cardWidth);
+      this.container.style.transform = `translateX(${this.totalMoveDistance}%)`;
+    }
+  }
+
+  handleClickable() {
+    this.enableClick = false;
+    setTimeout(() => {
+      this.enableClick = true;
+      this.container.style.transitionDuration = "0s";
+    }, this.speed);
   }
 }
